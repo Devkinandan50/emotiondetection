c529ff2b3be7aeba1c949856319a06a5163ea08f
Devkinandan50 <jagtapdevkinandan50@gmail.com>
2022-12-06 13:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 headings = ("Name","Album","Artist")
 df1 = music_rec()
 df1 = df1.head(15)
-@app.route('/video_feed')
+@app.route('/')
 def index():
     print(df1.to_json(orient='records'))
     return render_template('index.html', headings=headings, data=df1)
@@ -19,7 +19,7 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/')
+@app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
